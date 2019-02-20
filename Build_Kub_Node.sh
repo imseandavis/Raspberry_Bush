07d126db635c1d1b699b4a06c4930d9468ac5952
Sean Davis <imseandavis@gmail.com>
2019-02-19 21:15:45 -0500

    Update Build_Kub_Node.sh
    removed the initial setup steps and refactored to setup.sh

--- Build_Kub_Node.sh
+++ Build_Kub_Node.sh
@@ -1,31 +1,5 @@
 #!/bin/sh
 
-# Turn on SSH
-echo Turning on SSH...
-sudo touch /boot/ssh
-
-#Init Variables
-hostname=$1
-ip=$2 #IP
-gateway=$3 #IP Format
-dns=$4 #IP Format
-
-# Update Hostname
-echo Setting Hostname...
-sudo hostnamectl --transient set-hostname $hostname
-sudo hostnamectl --static set-hostname $hostname
-sudo hostnamectl --pretty set-hostname $hostname
-sudo sed -i 's/raspberrypi/$hostname/g' /etc/hosts
-
-# Set the static ip
-echo Setting Static IP to: $ip ...
-sudo cat <<EOT >> /etc/dhcpcd.conf
-interface eth0
-static ip_address=$ip/24
-static routers=$gateway
-static domain_name_servers=$dns
-EOT
-
 # Install Docker
 echo Installing Docker...
 curl -sSL get.docker.com | sh && \
@@ -50,12 +24,12 @@ sudo apt-get install -qy kubeadm
 
 #Download Config Files
 echo Downloading config files....
-sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Config_Kubernetes_Master.sh -o config.sh
+sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/config_kub_master.sh -o config.sh
 sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/kubeadm_conf.yaml -o kubadm_conf.yaml
 
 # Housekeeping
 echo Cleaning up....
-rm -f build_kub_node.sh
+rm -f setup.sh
 
 # Reboot
 echo Rebooting in 5 seconds...
