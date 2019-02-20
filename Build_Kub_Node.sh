aea5e7cfed0cafba7fa3b46a92bb55a41050fa71
Sean Davis <imseandavis@gmail.com>
2019-02-20 11:07:38 -0500

    Update Build_Kub_Node.sh
    Updated housekeeping step and echo description

--- Build_Kub_Node.sh
+++ Build_Kub_Node.sh
@@ -23,13 +23,13 @@ sudo apt-get update -q && \
 sudo apt-get install -qy kubeadm
 
 #Download Config Files
-echo Downloading config files....
+echo Downloading Master Node Config Files....
 sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/config_kub_master.sh -o config.sh
 sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/kubeadm_conf.yaml -o kubadm_conf.yaml
 
 # Housekeeping
 echo Cleaning up....
-rm -f setup.sh
+rm -f build_kub_node.sh
 
 # Reboot
 echo Rebooting in 5 seconds...
