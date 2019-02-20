4d4b5d6e87b83c5b0c3bafc699094f2ebfeedf84
Sean Davis <imseandavis@gmail.com>
2019-02-19 19:12:00 -0500

    Create Config_Kubernetes_Master.sh

---
+++ Config_Kubernetes_Master.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Download The Kub Config File From GitHub
+curl -sSL github.com/imseandavis/Raspberry_Bush/new/master/kubadmin_conf.yaml
+
+# Update Kub Config
+kubeadm init --config kubeadm_conf.yaml
+
+# Start Kub Cluster
+mkdir -p $HOME/.kube
+$ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+$ sudo chown $(id -u):$(id -g) $HOME/.kube/config
