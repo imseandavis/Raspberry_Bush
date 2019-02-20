838958c189c4c757b6c30c8e22311da44b068187
Sean Davis <imseandavis@gmail.com>
2019-02-20 15:14:37 -0500

    Update config_kube_master.sh
    added echo's and updated kube init command until new config file is needed.

--- config_kube_master.sh
+++ config_kube_master.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-# Update Kub Config
+# Update Kube Config
+echo Init KubeAdm...
 sudo kubeadm init # --config kubeadm_conf.yaml
 
-# Start Kub Cluster
+# Start Kube Cluster
+echo Start Kub Cluster...
 mkdir -p $HOME/.kube
-$ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-$ sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 #Verify Master Is Up
+echo Verify Kubernetes Master Node Is Up...
 kubectl get nodes
 
 # Housekeeping
+echo Do Some Housekeeping...
 rm -f config_kube_master.sh
