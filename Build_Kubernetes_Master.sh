4719bdd5595c302c9ad0198fbe139f492f18d173
Sean Davis <imseandavis@gmail.com>
2019-02-19 20:32:24 -0500

    Update Build_Kubernetes_Master.sh
    Added a few tweaks and housekeeping items.

--- Build_Kubernetes_Master.sh
+++ Build_Kubernetes_Master.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Turn on SSH
+echo Turning on SSH...
 sudo touch /boot/ssh
 
 #Set Hostname
@@ -9,12 +10,14 @@ ip=$2 # should be of format: 192.168.1.100
 dns=$3 # should be of format: 192.168.1.1
 
 # Change the hostname
+echo Setting Hostname...
 sudo hostnamectl --transient set-hostname $hostname
 sudo hostnamectl --static set-hostname $hostname
 sudo hostnamectl --pretty set-hostname $hostname
 sudo sed -i 's/raspberrypi/$hostname/g' /etc/hosts
 
 # Set the static ip
+echo Setting Static IP to: $ip ...
 sudo cat <<EOT >> /etc/dhcpcd.conf
 interface eth0
 static ip_address=$ip/24
@@ -23,10 +26,12 @@ static domain_name_servers=$dns
 EOT
 
 # Install Docker
+echo Installing Docker...
 curl -sSL get.docker.com | sh && \
 sudo usermod pi -aG docker
 
 # Disable Swap
+echo Disabling Swap File...
 sudo dphys-swapfile swapoff && \
 sudo dphys-swapfile uninstall && \
 sudo update-rc.d dphys-swapfile remove
@@ -36,10 +41,21 @@ orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
 # Add Repo List and Install KubeADM
+echo Installing KubeADM...
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
 sudo apt-get update -q && \
 sudo apt-get install -qy kubeadm
 
+#Download Config Files
+echo Downloading config files....
+sudo curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Config_Kubernetes_Master.sh -o config.sh
+
+# Housekeeping
+echo Cleaning up....
+rm -f build.sh
+
 # Reboot
+echo Rebooting in 5 seconds...
+sleep 5
 sudo reboot now
