fb64db912fe38004b06de972062f30bbf8231109
Sean Davis <imseandavis@gmail.com>
2019-02-19 19:03:02 -0500

    Create Build_Kubernetes_Master.sh

---
+++ Build_Kubernetes_Master.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#Set Hostname
+hostname=$1
+ip=$2 # should be of format: 192.168.1.100
+dns=$3 # should be of format: 192.168.1.1
+
+# Change the hostname
+sudo hostnamectl --transient set-hostname $hostname
+sudo hostnamectl --static set-hostname $hostname
+sudo hostnamectl --pretty set-hostname $hostname
+sudo sed -i s/raspberrypi/$hostname/g /etc/hosts
+
+# Set the static ip
+sudo cat <<EOT >> /etc/dhcpcd.conf
+interface eth0
+static ip_address=$ip/24
+static routers=$dns
+static domain_name_servers=$dns
+EOT
+
+# Install Docker
+curl -sSL get.docker.com | sh && \
+sudo usermod pi -aG docker
+
+# Disable Swap
+sudo dphys-swapfile swapoff && \
+sudo dphys-swapfile uninstall && \
+sudo update-rc.d dphys-swapfile remove
+echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
+sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
+echo $orig | sudo tee /boot/cmdline.txt
+
+# Add Repo List and Install KubeADM
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
+sudo apt-get update -q && \
+sudo apt-get install -qy kubeadm
