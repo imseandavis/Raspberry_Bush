22d68456b45a498eaf8ccbb3d9318142b38e1eea
Sean Davis <imseandavis@gmail.com>
2019-10-29 18:38:35 -0400

    Update worker_post_install_tasks.sh

--- Builds/k8s/worker_post_install_tasks.sh
+++ Builds/k8s/worker_post_install_tasks.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Download Demo Scripts
+echo Downloading Demo Scripts...
+curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Builds/k8s/demo_deploy.sh -o demo_deploy.sh
 
 # Cleanup Remaining Files
 rm -f post_install_tasks.sh
