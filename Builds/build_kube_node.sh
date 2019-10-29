9161872cd41598e7f40e7d14b099d61dec599eca
Sean Davis <imseandavis@gmail.com>
2019-10-29 13:56:31 -0400

    Update build_kube_node.sh

--- Builds/build_kube_node.sh
+++ Builds/build_kube_node.sh
@@ -28,8 +28,8 @@ do
 	M)
     echo Configuring Kube Master Node....
     
-    # Init KubeAdm
-    echo Init KubeAdm...
+    # Init KubeAdm, Set Token To Not Expire, and Use Flannel Pod Network For Add On
+    echo Init KubeAdm, Set Token To Not Expire, and Use Flannel Pod Network For Add On...
     sudo kubeadm init --token-ttl=0 --pod-network-cidr=10.244.0.0/16
             
     # Make Kube Cluster Available
@@ -51,12 +51,17 @@ do
     echo Verify Kubernetes Master Node Is Up and Ready...
     until kubectl get nodes | grep -E "Ready" -C 120; do sleep 5 | echo "Waiting For Node To Be Ready..."; done
     
+    # Write Token and SHA Hash To File
+    echo Writing Token and Hash To File...
+    kubeadm token list > token.txt
+    openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //' > hash.txt
+
     #End Master Node Configuration
     break;
     ;;
 	
 	S)
-
+   
     # Init Variables
     echo Configuring Kube Slave Node....
     echo
@@ -64,13 +69,14 @@ do
     echo
     echo Enter Kubernetes Master Host IP Address:
     read KubeMasterHostIP
-    echo Enter Kubernetes Master Host Token
-    read KubeMasterToken
-    echo Enter Kubernetes Master Host Hash
-    read KubeMasterHash
+        
+    #TODO: FIX RETRIEVAL CODE
+    echo Retrieving Token and Hash...
+    KubeMasterToken = echo yes | sudo ssh pi@KubMasterHostIP 'kubeadm token list'
+    KubeMasterHash = echo yes | sudo ssh pi@KubMasterHostIP "openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'"
     
     # Join Kubernetes Cluster
-    echo Joining Slave Node To Master Host $KubernetesMasterHostIP...
+    echo Joining Slave Node To Master Host $KubMasterHostIP...
     sudo kubeadm join $KubeMasterHostIP:6443 --token $KubeMasterToken --discovery-token-ca-cert-hash sha256:$KubeMasterHash
 
     #Verify All Nodes Are Up & Ready
@@ -79,7 +85,6 @@ do
     until kubectl get nodes | grep -E "Ready" -C 120; do sleep 5 | echo "Waiting For Node To Be Ready..."; done
     
     # TODO: Copy admin.conf / Secure kube.conf
-    # TODO: Pull Token On Slave Node From Master Node
 
     break;
     ;;
