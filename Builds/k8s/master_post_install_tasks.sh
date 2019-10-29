0a36b7c35aeb5fb5c4d17694ddd1df97d0f06ea6
Sean Davis <imseandavis@gmail.com>
2019-10-29 18:38:11 -0400

    Update master_post_install_tasks.sh

--- Builds/k8s/master_post_install_tasks.sh
+++ Builds/k8s/master_post_install_tasks.sh
@@ -12,5 +12,9 @@ kubectl get pods --all-namespaces
 # Check Cluster Nodes
 kubectl get nodes
 
+# Download Demo Scripts
+echo Downloading Demo Scripts...
+curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Builds/k3s/demo_deploy.sh -o demo_deploy.sh
+
 # Cleanup Remaining Files
 rm -f post_install_tasks.sh
