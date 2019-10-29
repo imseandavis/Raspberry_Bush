a51a0da49521e816fe8789e184454fa9d701f9e2
Sean Davis <imseandavis@gmail.com>
2019-10-29 19:28:53 -0400

    Update master_post_install_tasks.sh

--- Builds/k8s/master_post_install_tasks.sh
+++ Builds/k8s/master_post_install_tasks.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# Enable Cluster For User After Docker Group Change
+# Update Kube Admin Config To User Home
 echo Make Kube Cluster Available To Current User...
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-# Check Pod Network
-kubectl get pods --all-namespaces
-
-# Check Cluster Nodes
-kubectl get nodes
 
 # Download Demo Scripts
 echo Downloading Demo Scripts...
