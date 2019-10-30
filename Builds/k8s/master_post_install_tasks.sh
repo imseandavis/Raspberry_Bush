56b216d232997e74630173b2428576ae5cb6a40c
Sean Davis <imseandavis@gmail.com>
2019-10-29 21:36:43 -0400

    Update master_post_install_tasks.sh

--- Builds/k8s/master_post_install_tasks.sh
+++ Builds/k8s/master_post_install_tasks.sh
@@ -4,7 +4,8 @@
 echo Downloading Demo Scripts...
 curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Builds/k8s/demo_deploy.sh -o demo_deploy.sh
 echo
-echo Please Copy and paste each of the three commands below and run them before the cluster will be working and ready.
+echo You must run the three commands below before the cluster will be working and ready.
+echo
 echo mkdir -p $HOME/.kube
 echo sudo cp -i -f /etc/kubernetes/admin.conf $HOME/.kube/config
 echo sudo chown $(id -u):$(id -g) $HOME/.kube/config
