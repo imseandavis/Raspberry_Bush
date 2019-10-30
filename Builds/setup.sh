beb622da6682427ff6fa4a0cd71a122f8f6accbf
Sean Davis <imseandavis@gmail.com>
2019-10-29 20:30:08 -0400

    Update setup.sh

--- Builds/setup.sh
+++ Builds/setup.sh
@@ -42,7 +42,7 @@ done
 while :
 do
   case $nodeversion in
-	master,Master)
+	[Mm]aster)
     
 	# Download k8s Build Script
 	echo Set Master Build Flag...
@@ -50,7 +50,7 @@ do
 	break;
     ;;
 	
-	worker,Worker)
+	[Ww]orker)
    
     # Download k8s Build Script
 	echo Set Worker Build Flag...
