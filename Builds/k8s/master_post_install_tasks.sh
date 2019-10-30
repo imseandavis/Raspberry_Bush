907e0ef3f060d718d23f49222b78d895ccae0eba
Sean Davis <imseandavis@gmail.com>
2019-10-29 21:08:54 -0400

    Update master_post_install_tasks.sh

--- Builds/k8s/master_post_install_tasks.sh
+++ Builds/k8s/master_post_install_tasks.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Update Kube Admin Config To User Home
-echo Make Kube Cluster Available To Current User...
-mkdir -p $HOME/.kube
-sudo cp -f /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 # Download Demo Scripts
 echo Downloading Demo Scripts...
 curl -sSL https://raw.githubusercontent.com/imseandavis/Raspberry_Bush/master/Builds/k8s/demo_deploy.sh -o demo_deploy.sh
