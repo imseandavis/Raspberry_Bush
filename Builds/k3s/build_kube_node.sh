47a3cf14da084177cb0307f8a0ff9b38ff738d3f
Sean Davis <imseandavis@gmail.com>
2019-11-18 23:29:47 -0500

    Update build_kube_node.sh
    Updated worker/master split and added auto join config.

--- Builds/k3s/build_kube_node.sh
+++ Builds/k3s/build_kube_node.sh
@@ -1,25 +1,45 @@
 #!/bin/sh
 
-# Install k3s
-curl -sfL https://get.k3s.io | sh -
+#Build Node
+ if [ -f "master" ]
+   then 
+     # Install k3s
+     curl -sfL https://get.k3s.io | sh -
 
-# Check Status
-sudo systemctl status k3s
+     # Check k3s Status
+     sudo systemctl status k3s
 
-# Get Join Key
-sudo cat /var/lib/rancher/k3s/server/node-token
+     # Get Master Node Join Key
+     echo "";
+     echo "Master Join String - Keep This Somewhere Safe";
+     sudo cat /var/lib/rancher/k3s/server/node-token
 
+ elif [ -f "worker" ]
+   then
+     
+     # Build Exports To Join A Worker
+     echo Configuring Kube Worker Node....
+     echo
+     echo You will need your Kubernetes Master Host IP...
+     echo
+     echo Enter Kubernetes Master Host IP Address:
+     read KubeMasterHostIP
+     echo
+     echo Retrieving Kubernetes Master Join Token
+     export K3S_URL="https://$KubeMasterHostIP:6443"
+     export K3S_TOKEN=$(sudo ssh pi@$KubeMasterHostIP sudo cat /var/lib/rancher/k3s/server/node-token)
+     
+     #Checking k3s Node Status's On Server
+     sudo ssh pi@$KubeMasterHostIP sudo kubectl get node -o wide
 
-# Join A Worker
-#export K3S_URL="https://192.168.0.32:6443"
-#export K3S_TOKEN="K1089729d4ab5e51a44b1871768c7c04ad80bc6319d7bef5d94c7caaf9b0bd29efc::node:1fcdc14840494f3ebdcad635c7b7a9b7"
-#curl -sfL https://get.k3s.io | sh -
+ else echo "No Build Flag Found, Aborting Build!";
+ fi
 
 # Housekeeping
 #echo Doing A Little Housekeeping....
-#rm -f build_kube_node.sh
+rm -f build_kube_node.sh
 
 # Reboot
-#echo Rebooting in 10 seconds...
-#sleep 10
-#sudo reboot now
+echo Rebooting in 10 seconds...
+sleep 10
+sudo reboot now
